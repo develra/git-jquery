79994a3d8673c803428f8455d66b98020b9d0ddd
Develra <michael.drew.aaron@gmail.com>
2015-02-22 16:06:56 -0700

    50/50 all parts working

--- js/my/events.js
+++ js/my/events.js
@@ -1,7 +1,9 @@
 function myEvents(){
-    
-    // display a list of events belonging to me in #list
-    // e.g., https://api.github.com/users/doubleshow/events
-
-    alert('todo')
+       
+$.get("https://api.github.com/users/develra/events", github, function(events){
+$.get("/git-jquery/templates/eventList.jade", function(template){
+      var html = jade.render(template, {items: events})
+            $('#list').html(html)
+        })
+    })
 }
